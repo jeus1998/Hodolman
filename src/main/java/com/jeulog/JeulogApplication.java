b576a8f0e969dec7d4d518f1d575a693981263fa
jeus1998 <zeus20240228@gmail.com>
2024-08-16 12:23:42 +0900

    commit: create springboot project

---
+++ src/main/java/com/jeulog/JeulogApplication.java
@@ -0,0 +1,13 @@
+package com.jeulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeulogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeulogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeulog/JeulogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeulogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
