3adeee46ea84b650fafe45ee1bcc3a0c6dcf4fb6
jeus1998 <zeus20240228@gmail.com>
2024-08-16 14:13:13 +0900

    commit: PostController 수정

--- src/main/java/com/jeulog/controller/PostController.java
+++ src/main/java/com/jeulog/controller/PostController.java
@@ -1,16 +1,21 @@
 package com.jeulog.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import com.jeulog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 // SSR -> jsp, thymeleaf
         // -> html rendering
 // SPA -> vue
         // -> javascript + <-> API (JSON)
+
+@Slf4j
 @RestController
 public class PostController {
-    @GetMapping("/posts")
-    public String get(){
+    @PostMapping("/posts")
+    public String get(@RequestBody PostCreate dto){
+        log.info("dto={}", dto);
         return "Hello World";
     }
 }
