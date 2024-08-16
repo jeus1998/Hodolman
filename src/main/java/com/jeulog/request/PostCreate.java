21079e5151852b79f5b63d8732140f8cc5dabcec
jeus1998 <zeus20240228@gmail.com>
2024-08-16 14:13:51 +0900

    commit: PostCreate 생성 - DTO

---
+++ src/main/java/com/jeulog/request/PostCreate.java
@@ -0,0 +1,11 @@
+package com.jeulog.request;
+
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+public class PostCreate {
+    public String title;
+    public String content;
+}
