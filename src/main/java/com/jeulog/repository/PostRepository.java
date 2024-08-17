ac3d8fc4edce36b0dbcc56f7d46b4cdae252b2ce
jeus1998 <zeus20240228@gmail.com>
2024-08-17 21:00:20 +0900

    commit: PostRepository Post 엔티티 레포지토리 생성

---
+++ src/main/java/com/jeulog/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.jeulog.repository;
+
+import com.jeulog.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
