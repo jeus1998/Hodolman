67fe6992aac84ca031abd5c4d711dc2c478842b9
jeus1998 <zeus20240228@gmail.com>
2024-08-17 20:59:41 +0900

    commit: Post 엔티티 생성

---
+++ src/main/java/com/jeulog/domain/Post.java
@@ -0,0 +1,20 @@
+package com.jeulog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Lob
+    private String content;
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
