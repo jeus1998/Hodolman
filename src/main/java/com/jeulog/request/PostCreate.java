2c1215aaef0f0660566b974f5617951c82ab64b7
jeus1998 <zeus20240228@gmail.com>
2024-08-17 15:08:12 +0900

    test

--- src/main/java/com/jeulog/request/PostCreate.java
+++ src/main/java/com/jeulog/request/PostCreate.java
@@ -1,12 +1,17 @@
 package com.jeulog.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
 
 @ToString
 @Getter @Setter
 public class PostCreate {
+    @NotBlank(message = "타이틀을 입력해주세요")
     private String title;
+    @NotBlank(message = "컨텐트를 입력해주세요")
     private String content;
 }
