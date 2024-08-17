c7ecd25a7f93ffae05bf6f637f4c3c2dcbfcdd20
jeus1998 <zeus20240228@gmail.com>
2024-08-17 19:11:45 +0900

    commit ExceptionController 수정 return type ErrorResponse

--- src/main/java/com/jeulog/controller/ExceptionController.java
+++ src/main/java/com/jeulog/controller/ExceptionController.java
@@ -1,31 +1,30 @@
 package com.jeulog.controller;
 
+import com.jeulog.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e){
+    public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e){
         // MethodArgumentNotValidException
         log.error("error", e);
+        ErrorResponse response = new ErrorResponse("400", "잘못된 요청입니다.");
         List<FieldError> fieldErrors = e.getFieldErrors();
-        Map<String, String> response = new HashMap<>();
         for (FieldError fieldError : fieldErrors) {
-            String field = fieldError.getField();
-            String message = fieldError.getDefaultMessage();
-            response.put(field, message);
+            response.addValidation(fieldError.getField(), fieldError.getDefaultMessage());
         }
         return response;
     }
