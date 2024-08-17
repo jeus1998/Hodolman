3c31b0778a25f03b814bd01a63bb31f0894d592c
jeus1998 <zeus20240228@gmail.com>
2024-08-17 21:01:25 +0900

    commit: PostControllerTest 수정 , DB 저장 테스트 추가

--- src/test/java/com/jeulog/controller/PostControllerTest.java
+++ src/test/java/com/jeulog/controller/PostControllerTest.java
@@ -1,5 +1,7 @@
 package com.jeulog.controller;
 
+import com.jeulog.domain.Post;
+import com.jeulog.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,31 +10,25 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.*;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.assertj.core.api.Assertions.*;
 
-// @SpringBootTest
-// @AutoConfigureMockMvc
-@WebMvcTest
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+// WebMvcTest
 class PostControllerTest {
     @Autowired
     private MockMvc mockMvc;
-    @Test
-    @DisplayName("/posts 요청시 Hello World 출력")
-    void test() throws Exception {
-        // expected
-        mockMvc.perform(get("/posts"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"))
-                .andDo(print());
-    }
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Test
     @DisplayName("application/x-www-form-urlencoded")
@@ -43,8 +39,7 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("title", "글 제목 입니다")
                         .param("content", "글 내용 입니다"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"))
+                .andExpect(status().isUnsupportedMediaType()) // 415 @RequestBody 사용중
                 .andDo(print());
     }
     @Test
@@ -56,7 +51,7 @@ class PostControllerTest {
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"))
+                .andExpect(content().string("{}"))
                 .andDo(print());
     }
     @Test
@@ -73,6 +68,25 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.validations[0].errorMessage").value("타이틀을 입력해주세요"))
                 .andExpect(jsonPath("$.validations[1].errorMessage").value("컨텐트를 입력해주세요"))
                 .andDo(print());
+    }
+    @Test
+    @DisplayName("/post 요청시 DB에 값이 저장된다.")
+    void test5() throws Exception{
+        // when
+        mockMvc.perform(
+                post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\" }")
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // then
+        assertThat(postRepository.count()).isEqualTo(1);
+
+        List<Post> result = postRepository.findAll();
+        assertThat(result).extracting("title")
+                .containsExactly("제목입니다.");
     }
 
 }
