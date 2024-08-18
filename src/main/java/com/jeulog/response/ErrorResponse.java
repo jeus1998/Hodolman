ba897e2d45d17fcc3bf58043e31e696cf2192ce5
jeus1998 <zeus20240228@gmail.com>
2024-08-18 17:53:14 +0900

    commit: ErrorResponse @Builder 적용

--- src/main/java/com/jeulog/response/ErrorResponse.java
+++ src/main/java/com/jeulog/response/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.jeulog.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
@@ -15,11 +16,15 @@ import java.util.*;
  * }
  */
 @Getter
-@RequiredArgsConstructor
 public class ErrorResponse {
     private final String code;
     private final String message;
     private final List<Validation> validations = new ArrayList<>();
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
     public void addValidation(String fieldName, String errorMessage){
         validations.add(new Validation(fieldName, errorMessage));
     }
