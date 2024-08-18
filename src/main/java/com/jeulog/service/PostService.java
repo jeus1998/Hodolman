fa38a20d1a35b9eb75a863fba357d0f0b542391b
jeus1998 <zeus20240228@gmail.com>
2024-08-18 18:02:09 +0900

    commit: PostService post 엔티티 생성자 생성 -> 빌더 생성 (변경)

--- src/main/java/com/jeulog/service/PostService.java
+++ src/main/java/com/jeulog/service/PostService.java
@@ -14,7 +14,12 @@ public class PostService {
     private final PostRepository postRepository;
     public void write(PostCreate postCreate){
         // postCreate -> Entity
-        Post post = new Post(postCreate.getTitle(), postCreate.getContent());
+        Post post = Post
+                .builder()
+                .title(postCreate.getTitle())
+                .content(postCreate.getContent())
+                .build();
+
         postRepository.save(post);
     }
 }
