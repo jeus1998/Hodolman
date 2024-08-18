2e60bf453af248d85f742a17b2cafabeaec6b31d
jeus1998 <zeus20240228@gmail.com>
2024-08-18 17:48:49 +0900

    commit: PostCreate @Builder - 빌더 패턴 적용

--- src/main/java/com/jeulog/request/PostCreate.java
+++ src/main/java/com/jeulog/request/PostCreate.java
@@ -1,9 +1,7 @@
 package com.jeulog.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @ToString
 @Getter @Setter
@@ -12,4 +10,14 @@ public class PostCreate {
     private String title;
     @NotBlank(message = "컨텐트를 입력해주세요")
     private String content;
+    @Builder
+    public PostCreate(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+    // 빌더의 장점
+    // 가독성에 좋다 (값 생성에 대한 유연한)
+    // 필요한 값만 받을 수 있다. -> 생성자 오버로딩이 필요 X
+    // 객체의 불변성 (final 지정 가능)
+    // 생성자 주입 실수를 안한다.
 }
