2dc9dbcd0037ebc007c0988c8c8936faf5291d2d
jeus1998 <zeus20240228@gmail.com>
2024-08-19 22:08:59 +0900

    commit: PostRepositoryImpl Querydsl 사용 구현 클래스

---
+++ src/main/java/com/jeulog/repository/PostRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.jeulog.repository;
+
+import com.jeulog.domain.Post;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import static com.jeulog.domain.QPost.*;
+
+@RequiredArgsConstructor
+public class PostRepositoryImpl implements PostRepositoryCustom{
+    private final JPAQueryFactory queryFactory;
+    @Override
+    public List<Post> getList(int page) {
+         return queryFactory
+                 .selectFrom(post)
+                 .limit(10)
+                 .offset((page - 1) * 10)
+                 .fetch();
+    }
+}
