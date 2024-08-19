11a4379f57bdaec8e77ffba1af9e90c6a4510105
jeus1998 <zeus20240228@gmail.com>
2024-08-19 22:51:07 +0900

    commit: PostRepositoryCustom 매개변수 수정

--- src/main/java/com/jeulog/repository/PostRepositoryCustom.java
+++ src/main/java/com/jeulog/repository/PostRepositoryCustom.java
@@ -1,8 +1,10 @@
 package com.jeulog.repository;
 
 import com.jeulog.domain.Post;
+import com.jeulog.request.PostSearch;
+
 import java.util.List;
 
 public interface PostRepositoryCustom {
-    List<Post> getList(int page);
+    List<Post> getList(PostSearch postSearch);
 }
