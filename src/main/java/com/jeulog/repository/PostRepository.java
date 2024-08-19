600859b0cd54f6f2fea2b82deb85b369fe5856c1
jeus1998 <zeus20240228@gmail.com>
2024-08-19 22:09:37 +0900

    commit: PostRepository PostRepositoryCustom extends

--- src/main/java/com/jeulog/repository/PostRepository.java
+++ src/main/java/com/jeulog/repository/PostRepository.java
@@ -3,5 +3,6 @@ package com.jeulog.repository;
 import com.jeulog.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
+
 }
