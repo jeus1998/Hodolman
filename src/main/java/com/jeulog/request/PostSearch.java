017664466db231117418abec86c6de2fcb7fff2f
jeus1998 <zeus20240228@gmail.com>
2024-08-19 23:12:01 +0900

    commit: PostSearch 수정 -> page & limit -> null 이면 NPE 발생

--- src/main/java/com/jeulog/request/PostSearch.java
+++ src/main/java/com/jeulog/request/PostSearch.java
@@ -8,13 +8,17 @@ import static java.lang.Math.*;
 
 @Getter
 @Setter
-@Builder
 public class PostSearch {
     private static final int MAX_SIZE = 2000;
-    @Builder.Default
-    private Integer page = 1;
-    @Builder.Default
-    private Integer size = 10;
+    private Integer page;
+    private Integer size;
+    // 생성자에서 null 체크를 하도록 변경
+    @Builder
+    public PostSearch(Integer page, Integer size) {
+        this.page = page == null ? 1 : page;
+        this.size = size == null ? 10 : size;
+    }
+
     public long getOffset(){
         return (long) (max(1, page) - 1) * min(size, MAX_SIZE);
     }
