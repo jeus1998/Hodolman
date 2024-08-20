6d42b619ceac3201f8cc7ecf6b66caa437564daf
jeus1998 <zeus20240228@gmail.com>
2024-08-20 20:23:32 +0900

    commit: InvalidRequest filed 에러 추가

--- src/main/java/com/jeulog/exception/InvalidRequest.java
+++ src/main/java/com/jeulog/exception/InvalidRequest.java
@@ -5,6 +5,10 @@ public class InvalidRequest extends JeuException{
     public InvalidRequest() {
         super(MESSAGE);
     }
+    public InvalidRequest(String fieldName, String errorMessage) {
+        super(MESSAGE);
+        super.addValidations(fieldName, errorMessage);
+    }
     @Override
     public int statusCode() {
         return 400; // BAD REQUEST
