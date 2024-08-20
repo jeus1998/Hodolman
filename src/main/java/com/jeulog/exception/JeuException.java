65ccad74617cf92847227fa1c41cf0351b1cf51f
jeus1998 <zeus20240228@gmail.com>
2024-08-20 20:22:49 +0900

    commit: JeuException 수정

--- src/main/java/com/jeulog/exception/JeuException.java
+++ src/main/java/com/jeulog/exception/JeuException.java
@@ -1,8 +1,23 @@
 package com.jeulog.exception;
 
+import com.jeulog.response.ErrorResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class JeuException extends RuntimeException {
+    private final List<Validation> validations = new ArrayList<>();
     public JeuException(String message) {
         super(message);
     }
+    public List<Validation> getValidations(){
+        return validations;
+    }
     public abstract int statusCode();
+    public void addValidations(String fieldName, String errorMessage){
+        validations.add(Validation.builder()
+                        .fieldName(fieldName)
+                        .errorMessage(errorMessage)
+                        .build());
+    }
 }
