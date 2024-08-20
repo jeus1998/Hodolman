41841638ab335451bc67c5ad3bc36079c3b433a6
jeus1998 <zeus20240228@gmail.com>
2024-08-20 19:29:43 +0900

    commit: PostNotFound 수정

--- src/main/java/com/jeulog/exception/PostNotFound.java
+++ src/main/java/com/jeulog/exception/PostNotFound.java
@@ -1,7 +1,11 @@
 package com.jeulog.exception;
-public class PostNotFound extends RuntimeException {
+public class PostNotFound extends JeuException {
     private static final String MESSAGE = "존재하지 않는 글입니다.";
     public PostNotFound() {
         super(MESSAGE);
+    }
+    @Override
+    public int statusCode() {
+        return 404; // NOT FOUND
     }
 }
