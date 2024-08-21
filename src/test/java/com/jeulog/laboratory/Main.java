060612a286541c306ee8ea55c8a58aae5c803b55
jeus1998 <zeus20240228@gmail.com>
2024-08-21 13:04:44 +0900

    commit: final, abstract, private 공부

---
+++ src/test/java/com/jeulog/laboratory/Main.java
@@ -0,0 +1,7 @@
+package com.jeulog.laboratory;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utility.plus(1,2));
+    }
+}
---
+++ src/test/java/com/jeulog/laboratory/Utility.java
@@ -0,0 +1,9 @@
+package com.jeulog.laboratory;
+
+public final class Utility {
+    private Utility() {
+    }
+    static int plus(int a, int b){
+        return a + b;
+    }
+}
