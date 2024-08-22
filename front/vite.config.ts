fc70e3d48e3d76a6ea28deaa85205af3ae145345
jeus1998 <zeus20240228@gmail.com>
2024-08-22 22:38:58 +0900

    commit: vite.config.ts CORS 해결을 위해 프록시 설정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
