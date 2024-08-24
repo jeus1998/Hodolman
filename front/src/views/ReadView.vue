6a98d30a63e1969fe478f92d6050bb37585566d7
jeus1998 <zeus20240228@gmail.com>
2024-08-24 11:51:33 +0900

    commit: ReadView.vue 글 상세 생성 - 홈에서 넘어오면 axios 비동기 통신을 통해 서버에서 게시글 조회

---
+++ front/src/views/ReadView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import axios from "axios";
+import {onMounted, ref} from "vue";
+
+const props = defineProps({
+  postId: {
+    type: [Number, String],
+    required: true,
+  }
+});
+
+const post = ref({
+  id: 0,
+  title: "",
+  content: ""
+});
+
+onMounted(() => {
+  // console.log(props.postId)
+  axios.get(`/api/posts/${props.postId}`)
+      .then((response) => {
+        // console.log(response)
+        post.value = response.data;
+      });
+});
+</script>
+
+<template>
+  <h2>{{post.title}}</h2>
+  <div>{{post.content}}</div>
+</template>
