da94ed462c3736f52a7df91fc6f29f23b39e45a9
jeus1998 <zeus20240228@gmail.com>
2024-08-24 11:50:39 +0900

    commit: HomeView.vue 글 제목을 누르면 글 상세로 이동하도록 수정

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
 import axios from "axios";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter()
 
 const posts = ref([]);
 
 axios.get("/api/posts?page=1&size=10")
-    .then(response => {
-      console.log(response);
-       response.data.forEach((r:any) => {
-          posts.value.push(r);
-       });
-    });
+  .then(response => {
+    console.log(response);
+     response.data.forEach((r:any) => {
+        posts.value.push(r);
+     });
+  });
+
+const moveToRead = () =>{
+   router.push({ name: "read" });
+};
+
 </script>
 
 <template>
    <ul>
      <li v-for="post in posts" :key="post.id">
         <div>
-            {{post.title}}
+            <router-link :to="{ name: 'read', params: {postId: post.id} }">{{post.title}}</router-link>
         </div>
+
         <div>
             {{post.content}}
         </div>
