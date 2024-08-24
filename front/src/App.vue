ef7465d15286733e8ece51eeb41ee812ff8ba4c2
jeus1998 <zeus20240228@gmail.com>
2024-08-24 14:43:20 +0900

    commit: App.vue 콘텐트랑 헤더 간격 수정

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,14 +5,22 @@ import Content from "@/components/Content.vue";
 
 <template>
   <Header />
-  <Content />
+  <div class="main-content">
+      <Content />
+    </div>
 </template>
 
 <style>
+
 body {
   font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
     "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
     "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
     sans-serif;
+  margin: 0; /* 기본 여백 제거 */
+}
+
+.main-content {
+  padding-top: 60px; /* 헤더 높이와 동일한 패딩 추가 */
 }
 </style>
