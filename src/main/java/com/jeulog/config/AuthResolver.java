e96dc59fd6e064669da8c070b4e9d9b6b5d670a4
jeus1998 <zeus20240228@gmail.com>
2024-08-25 22:47:06 +0900

    commit: 쿠키를 통한 인증 및 검증

--- src/main/java/com/jeulog/config/AuthResolver.java
+++ src/main/java/com/jeulog/config/AuthResolver.java
@@ -4,14 +4,17 @@ import com.jeulog.config.data.UserSession;
 import com.jeulog.domain.Session;
 import com.jeulog.exception.Unauthorized;
 import com.jeulog.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
     private final SessionRepository sessionRepository;
@@ -21,9 +24,15 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     }
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
-        if(!StringUtils.hasText(accessToken)) throw new Unauthorized();
+        Cookie[] cookies = request.getCookies();
+        if(cookies.length == 0){
+            log.error("쿠키가 없음");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         // 데이터베이스 사용자 확인 작업
         Session session = sessionRepository.findByAccessToken(accessToken).orElseThrow(Unauthorized::new);
--- src/main/java/com/jeulog/controller/AuthController.java
+++ src/main/java/com/jeulog/controller/AuthController.java
@@ -5,9 +5,14 @@ import com.jeulog.response.SessionResponse;
 import com.jeulog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -15,7 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login){
-        return new SessionResponse(authService.login(login));
+    public ResponseEntity login(@RequestBody Login login){
+        String accessToken = authService.login(login);
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info("cookie={}", cookie);
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
