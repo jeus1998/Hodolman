162590cc3081b78c2c7303a3a19f4519ce6de6da
jeus1998 <zeus20240228@gmail.com>
2024-08-25 21:44:51 +0900

    commit: User 빌더, Session 연관관계 추가, 연관관계 편의 메서드 생성

--- src/main/java/com/jeulog/domain/User.java
+++ src/main/java/com/jeulog/domain/User.java
@@ -1,19 +1,40 @@
 package com.jeulog.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.util.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 @Table(name = "users")
+@ToString(of ={"id", "name", "email", "password", "createdAt"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
     private String name;
     private String email;
     private String password;
     private LocalDateTime createdAt;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Session> sessions = new ArrayList<>();
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+    public Session addSession(){
+        Session session = Session.builder()
+                .user(this)
+                .accessToken(UUID.randomUUID().toString())
+                .build();
+        sessions.add(session);
+        return session;
+    }
 }
