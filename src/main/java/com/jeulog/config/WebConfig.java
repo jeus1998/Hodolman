94e1862b38ccd975e00f7232277c53cbe51b7fc2
jeus1998 <zeus20240228@gmail.com>
2024-08-25 15:25:15 +0900

    commit: WebConfig 생성한 인증 Resolver 등록

--- src/main/java/com/jeulog/config/WebConfig.java
+++ src/main/java/com/jeulog/config/WebConfig.java
@@ -1,9 +1,12 @@
 package com.jeulog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,7 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor())
-                .excludePathPatterns("/test2");
+       // registry.addInterceptor(new AuthInterceptor()).excludePathPatterns("/error", "/favicon.ico");
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+       resolvers.add(new AuthResolver());
     }
 }
