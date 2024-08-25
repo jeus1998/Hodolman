3c5921aeabb1f82878494c1709b5eda4fc193d92
jeus1998 <zeus20240228@gmail.com>
2024-08-25 11:59:39 +0900

    commit: WebConfig 인증 인터셉처 추가

--- src/main/java/com/jeulog/config/WebConfig.java
+++ src/main/java/com/jeulog/config/WebConfig.java
@@ -2,13 +2,21 @@ package com.jeulog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173");
+        // registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+    }
+    /**
+     * 인터셉터 등록하기
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .excludePathPatterns("/test2");
     }
 }
