8be0cad23cb68184815ef110ac692cd8910228e5
jeus1998 <zeus20240228@gmail.com>
2024-08-25 21:43:49 +0900

    commit: AuthService 생성

---
+++ src/main/java/com/jeulog/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.jeulog.service;
+
+import com.jeulog.domain.Session;
+import com.jeulog.domain.User;
+import com.jeulog.exception.InvalidSignInformation;
+import com.jeulog.repository.UserRepository;
+import com.jeulog.request.Login;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    @Transactional
+    public String login(Login login){
+        log.info("AuthService");
+        User user = userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword())
+                       .orElseThrow(InvalidSignInformation::new);
+
+        return user.addSession().getAccessToken();
+    }
+}
