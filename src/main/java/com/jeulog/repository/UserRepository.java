e18e35eac44a0d13c3172579277dc7392d0ad6bf
jeus1998 <zeus20240228@gmail.com>
2024-08-25 16:59:21 +0900

    commit: UserRepository 생성

---
+++ src/main/java/com/jeulog/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jeulog.repository;
+
+import com.jeulog.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAndPassword(@Param("Email") String Email, @Param("Password") String Password);
+}
