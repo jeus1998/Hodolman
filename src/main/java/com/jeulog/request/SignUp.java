cdafbd9555d4a284605af47135544ef1fba36c43
jeus1998 <zeus20240228@gmail.com>
2024-08-28 13:55:06 +0900

    commit: SignUp 회원가입 DTO 생성

---
+++ src/main/java/com/jeulog/request/SignUp.java
@@ -0,0 +1,12 @@
+package com.jeulog.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SignUp {
+    private String name;
+    private String password;
+    private String email;
+}
