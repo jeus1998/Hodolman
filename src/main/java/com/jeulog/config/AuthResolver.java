a1066fa859cc7972805c1bc61b523e2ca115869c
jeus1998 <zeus20240228@gmail.com>
2024-08-28 13:54:45 +0900

    commit: AuthResolver 코드 정리

--- src/main/java/com/jeulog/config/AuthResolver.java
+++ src/main/java/com/jeulog/config/AuthResolver.java
@@ -1,19 +1,14 @@
 package com.jeulog.config;
 
 import com.jeulog.config.data.UserSession;
-import com.jeulog.controller.AuthController;
-import com.jeulog.domain.Session;
 import com.jeulog.exception.Unauthorized;
 import com.jeulog.repository.SessionRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.MethodParameter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -43,6 +38,7 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
                     .setSigningKey(appConfig.getJwtKey())
                     .build()
                     .parseClaimsJws(jws);
+
             String userId = claims.getPayload().getSubject();
             return new UserSession(Long.parseLong(userId));
         }
