8948cb0866025d91853d343d524cc88bb38d3d46
jeus1998 <zeus20240228@gmail.com>
2024-08-28 09:28:14 +0900

    commit: @ConfigurationProperties 연습

--- src/main/java/com/jeulog/JeulogApplication.java
+++ src/main/java/com/jeulog/JeulogApplication.java
@@ -1,13 +1,14 @@
 package com.jeulog;
 
+import com.jeulog.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(AppConfig.class)
 @SpringBootApplication
 public class JeulogApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JeulogApplication.class, args);
     }
-
 }
