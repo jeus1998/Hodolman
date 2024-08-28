59316c955f1d559cbf8b4919095ddfacb77acb90
jeus1998 <zeus20240228@gmail.com>
2024-08-28 13:55:54 +0900

    commit: AuthService 회원가입 추가

--- src/main/java/com/jeulog/service/AuthService.java
+++ src/main/java/com/jeulog/service/AuthService.java
@@ -2,13 +2,20 @@ package com.jeulog.service;
 
 import com.jeulog.domain.Session;
 import com.jeulog.domain.User;
+import com.jeulog.exception.AlreadyExistsEmailException;
+import com.jeulog.exception.InvalidRequest;
 import com.jeulog.exception.InvalidSignInformation;
+import com.jeulog.exception.Unauthorized;
 import com.jeulog.repository.UserRepository;
 import com.jeulog.request.Login;
+import com.jeulog.request.SignUp;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.rmi.AlreadyBoundException;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -22,5 +29,19 @@ public class AuthService {
                        .orElseThrow(InvalidSignInformation::new);
 
         return user.getId();
+    }
+    public void signUp(SignUp signup) {
+
+        Optional<User> findDuplicateUser = userRepository.findByEmail(signup.getEmail());
+        if(findDuplicateUser.isPresent()){
+            throw new AlreadyExistsEmailException();
+        }
+
+        User user = User.builder()
+                .name(signup.getName())
+                .password(signup.getPassword())
+                .email(signup.getEmail())
+                .build();
+        userRepository.save(user);
     }
 }
