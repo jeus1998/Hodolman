4ec5e5dd05bb4f1e3338d0f97493586f9f190904
jeus1998 <zeus20240228@gmail.com>
2024-08-29 23:30:52 +0900

    commit: AuthController 수정

--- src/main/java/com/jeulog/controller/AuthController.java
+++ src/main/java/com/jeulog/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.jeulog.request.SignUp;
 import com.jeulog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
     private final AppConfig appConfig;
+    @GetMapping("/auth/login")
+    public String login(){
+        return "로그인 페이지 입니다";
+    }
     @PostMapping("/auth/signup")
     public void signup(@RequestBody SignUp signUp){
         authService.signUp(signUp);
