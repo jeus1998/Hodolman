af2cde2fcb252f6265c340798863a5053cfede39
jeus1998 <zeus20240228@gmail.com>
2024-09-02 18:34:39 +0900

    commit: SecurityConfig: 비밀번호 암호화 + UserDetailsService 추가, FormLogin 관련 설정

--- src/main/java/com/jeulog/config/SecurityConfig.java
+++ src/main/java/com/jeulog/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
@@ -25,10 +31,34 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
-                .authorizeHttpRequests(authorize -> authorize
+                .authorizeHttpRequests(
+                        authorize -> authorize
                         .requestMatchers("/auth/login").permitAll()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable) // todo: csrf? 공부하기 or (csrf -> csrf.disable())
+                .formLogin(login -> login
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/auth/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/")
+                )
+                .userDetailsService(userDetailService())
                 .build();
+    }
+    @Bean
+    public UserDetailsService userDetailService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        UserDetails user =
+                User.withUsername("baejeu")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+        manager.createUser(user);
+        return manager;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
