dc64fc3f2ba12980d5726963579d310ae09cdb6c
jeus1998 <zeus20240228@gmail.com>
2024-09-03 12:07:34 +0900

    commit: 시큐리티 로그인 사용자 정보 가져오기 @AuthenticationPrincipal

--- src/main/java/com/jeulog/controller/MainController.java
+++ src/main/java/com/jeulog/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.jeulog.controller;
 
+import com.jeulog.config.UserPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +12,7 @@ public class MainController {
         return "메인 페이지";
     }
     @GetMapping("/user")
-    public String user(){
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal){
         return "사용자 페이지입니다.";
     }
     @GetMapping("/admin")
